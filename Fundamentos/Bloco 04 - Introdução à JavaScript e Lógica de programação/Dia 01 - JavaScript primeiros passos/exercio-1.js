e19987da821175fd22090946d137a625ec95e966
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2021-11-29 21:43:49 -0300

    Exercício 1 - 5 progrmas de operações aritmética básica.

---
+++ Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de programação/Dia 01 - JavaScript primeiros passos/exercio-1.js
@@ -0,0 +1,19 @@
+const numberA = 100;
+const numberB = 5;
+
+console.log('Soma: ' + (numberA + numberB));
+
+/*let adicao = numberA + numberB;
+console.log(adicao);*/
+
+let subtracao = numberA - numberB;
+console.log('Subtração: ' + subtracao);
+
+let multiplicacao = numberA * numberB;
+console.log('Multiplicação: ' + multiplicacao);
+
+let divisao = numberA / numberB;
+console.log('Divisão: ' + divisao);
+
+let modulo = numberA % numberB;
+console.log('Modulo: ' + modulo);
