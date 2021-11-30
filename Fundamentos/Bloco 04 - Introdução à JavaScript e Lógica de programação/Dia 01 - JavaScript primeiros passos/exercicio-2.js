346b77b4fdc694b01102575e84a25e7895254d9c
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2021-11-29 21:54:21 -0300

    Exercício 2 - retornar o maior de dois números

---
+++ Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de programação/Dia 01 - JavaScript primeiros passos/exercicio-2.js
@@ -0,0 +1,10 @@
+const number1 = 50;
+const number2 = 80;
+
+
+
+if (number1 > number2) {
+    console.log("O número maior é " + number1 )
+} else {
+    console.log("O numero maior é " + number2)
+}
