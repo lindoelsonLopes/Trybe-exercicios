d1dfbd8c08b8bd74f65122c522018a105f6e0883
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2021-11-29 22:06:07 -0300

    exercício 3 - Retorne o maior de três números.

---
+++ Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de programação/Dia 01 - JavaScript primeiros passos/exercicio-3.js
@@ -0,0 +1,11 @@
+const numeracaoA = 50;
+const numeracaoB = 60;
+const numeracaoC = 70;
+
+if (numeracaoA > numeracaoB && numeracaoA > numeracaoC) {
+    console.log("O número maior é " + numeracaoA);
+} else if (numeracaoB > numeracaoA && numeracaoB > numeracaoC) {
+    console.log("O número maior é " + numeracaoB);
+} else {
+    console.log ("O número maior é " + numeracaoC);
+}
