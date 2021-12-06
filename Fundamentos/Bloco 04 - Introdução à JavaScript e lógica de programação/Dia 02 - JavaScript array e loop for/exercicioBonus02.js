4bba4938a317b0b40ba6ea37e4c6bb2022dfa78e
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2021-12-06 10:23:05 -0300

    Exercício Bônus 2 - Ordenar um array em ordem decrescente

---
+++ Fundamentos/Bloco 04 - Introdução à JavaScript e lógica de programação/Dia 02 - JavaScript array e loop for/exercicioBonus02.js
@@ -0,0 +1,12 @@
+let number = [45, 27, 57, 1330, 560, 800, 200, 70, 945, 54];
+for (let index = 1; index < number.length; index += 1) {
+    for (let secondIndex = 0; secondIndex < index; secondIndex += 1) {
+      if (number[index] > number[secondIndex]) {
+        let position = number[index];
+        number[index] = number[secondIndex];
+        number[secondIndex] = position;
+      }
+    }
+  }
+
+  console.log(number);
