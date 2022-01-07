d70b3c8ec5a7418de4f217c7dd023f718866d9c5
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-01-07 10:49:34 -0300

    Exercício 2 e 3 - imprimir objeto com for/in

--- Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de Programação/Dia 04 - JavaScript objetos e funções/script.js
+++ Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de Programação/Dia 04 - JavaScript objetos e funções/script.js
@@ -4,8 +4,20 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: 'Sim'
   };
 
   // 1. Imprima no console uma mensagem de boas-vindas para a personagem acima, incluindo seu nome. Valor esperado no console.
 
   console.log('Bem-vinda, ' + info.personagem);
+
+  // 2. Insira no objeto uma nova propriedade com o nome de chave 'recorrente' e o valor 'Sim' e, em seguida, imprima o objeto no console. Valor esperado no console:
+
+  console.log(info);
+
+  // 3. Faça um for/in que mostre todas as chaves do objeto. Valor esperado no console:
+
+  for(let index in info){
+      console.log(index);
+  }
+
