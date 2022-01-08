6776e4e469990492d5c2dbcd9314fee099b83e7e
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-01-08 12:44:33 -0300

    Exercícios 4 de objetos

--- Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de Programação/Dia 04 - JavaScript objetos e funções/script.js
+++ Fundamentos/Bloco 04 - Introdução à JavaScript e Lógica de Programação/Dia 04 - JavaScript objetos e funções/script.js
@@ -26,3 +26,6 @@ let info = {
   for(let index in info){
     console.log(info[index]);
 }
+
+// 5. Agora, defina um segundo objeto com a mesma estrutura (as mesmas chaves) do primeiro e os seguintes valores: 'Tio Patinhas', 'Christmas on Bear Mountain, Dell's Four Color Comics #178', 'O último MacPatinhas', 'Sim'. Então, imprima os valores de cada objeto juntos de acordo com cada uma das chaves. Valor esperado no console:
+
