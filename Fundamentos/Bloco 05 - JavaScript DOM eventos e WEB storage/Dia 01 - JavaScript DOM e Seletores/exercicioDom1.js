e0c79217c2d6fdf35f15f889b8afa98faf966056
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-01-09 15:28:11 -0300

    Finalização dos exercícios de DOM

--- Fundamentos/Bloco 05 - JavaScript DOM eventos e WEB storage/Dia 01 - JavaScript DOM e Seletores/exercicioDom1.js
+++ Fundamentos/Bloco 05 - JavaScript DOM eventos e WEB storage/Dia 01 - JavaScript DOM e Seletores/exercicioDom1.js
@@ -29,3 +29,23 @@ function corrigirTexto(){
 };
 
 corrigirTexto();
+
+function uppercase () {
+    let letterUppercase = document.getElementsByTagName('p');
+    for(let index = 0; index < letterUppercase.length; index +=1){
+           letterUppercase[index].innerHTML = letterUppercase[index].innerHTML.toUpperCase(); 
+    }
+    
+}
+
+uppercase();
+
+function tagConsole () {
+    let consoleTag = document.getElementsByTagName('p');
+    for(let index = 0; index < consoleTag.length; index +=1){
+          console.log(consoleTag[index].innerHTML); 
+    }
+    
+}
+
+tagConsole();
