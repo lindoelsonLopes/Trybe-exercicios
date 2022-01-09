07fe6b4364dc18aeed98878d632c14a20fc06edf
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-01-09 13:32:26 -0300

    Exercícios 04 e 05 - manipulação de DOM

--- Fundamentos/Bloco 05 - JavaScript DOM eventos e WEB storage/Dia 01 - JavaScript DOM e Seletores/exercicioDom1.js
+++ Fundamentos/Bloco 05 - JavaScript DOM eventos e WEB storage/Dia 01 - JavaScript DOM e Seletores/exercicioDom1.js
@@ -2,7 +2,7 @@
 
 function mudarTexto(){
     let tagp = document.getElementsByTagName('p')[0];
-    tagp.innerText = 'Trabalhando em uma empresa muito bacana'
+    tagp.innerText = 'Trabalhando em uma empresa muito bacana!'
 };
 
 mudarTexto();
@@ -15,3 +15,17 @@ function mudarCor(){
 }
 
 mudarCor();
+
+function mudarCorVermelho(){
+    let white = document.querySelectorAll('.center-content')[0];
+    white.style.backgroundColor = 'white';
+}
+
+mudarCorVermelho();
+
+function corrigirTexto(){
+    let title = document.getElementsByTagName('h1')[0];
+    title.innerText = 'Exercício 5.1 - JavaScript'
+};
+
+corrigirTexto();
