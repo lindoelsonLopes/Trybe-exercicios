1badc0e66009d9b1398a43085f5e99e4798f75a1
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-02-16 19:12:40 -0300

    Realizando exercícios de exemplo com Classes e React

---
+++ Front-End/Bloco10_Introdução_à_React/sobre/src/App.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import ReactClass from './Component';
+
+function App() {
+  return <ReactClass/>;
+}
+
+export default App;
---
+++ Front-End/Bloco10_Introdução_à_React/sobre/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class ReactClass extends Component {
+  render() {
+    return (
+        <div className='App-header'>
+          <h1>Lindoelson Lopes</h1>
+          <p>Nascido em São Paulo, 41 anos e apaixonado por tecnologia.</p>
+        </div>
+    )
+  }
+}
+
+export default ReactClass;
