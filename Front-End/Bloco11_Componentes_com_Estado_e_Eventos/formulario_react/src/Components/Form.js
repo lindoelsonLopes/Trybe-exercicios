700d3b404fcd87d69f75fd4869379ab75da2f5b4
Lindoelson Lopes <lindoelson_lopes@hotmail.com>
2022-02-25 10:52:31 -0300

    Criando event handlers genéricos e ajustando checkbox

--- Front-End/Bloco11_Componentes_com_Estado_e_Eventos/formulario_react/src/Components/Form.js
+++ Front-End/Bloco11_Componentes_com_Estado_e_Eventos/formulario_react/src/Components/Form.js
@@ -5,19 +5,38 @@ class Form extends React.Component {
   constructor() {
     super()
 
-    this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+    // this.handleTextAreaChange = this.handleTextAreaChange.bind(this);
+    // this.handleEmailChange = this.handleEmailChange.bind(this);
+    this.handleChange = this.handleChange.bind(this);
 
     this.state = {
       estadoFavorito: '',
       email: '',
       name: '',
-      select: ''
+      select: '',
+      validar: 'false'
     }
   }
 
-  handleTextAreaChange(event) {
-    this.setState({ estadoFavorito: event.target.value }) 
+//   handleTextAreaChange(event) {
+//     this.setState({ estadoFavorito: event.target.value }) 
+//   }
+
+//   handleEmailChange(event) {
+//     this.setState({ email: event.target.value }) 
+//   }
+
+// event handlers genéricos
+
+  handleChange({ target }) {
+    const { name } = target
+    const value = target.type === 'checkbox' ? target.checked : target.value
+
+    this.setState({
+      [name]: value
+    })
   }
+
 
     render() {
         return (
@@ -28,32 +47,58 @@ class Form extends React.Component {
               id="favorito" 
               name="estadoFavorito"
               value={this.state.estadoFavorito}
-              onChange={this.handleTextAreaChange}
+              onChange={this.handleChange}
             />
             </label>
 
             <div> 
             <label>
                 E-mail
-                <input name="email" />
+                <input
+                type="email"
+                name="email"
+                value={this.state.email}
+                onChange={this.handleChange}
+            />
             </label>
             </div>
 
             <div>
             <label>
                 Nome
-                <input name="name" />
+                <input
+                type="text"
+                name="name"
+                value={this.state.name}
+                onChange={this.handleChange}
+                />
             </label>
             </div>
 
             <div>
             <label>
                 Estado
-                <select name="select">
-                <option value="saoPaulo" selected>Escolher</option>
-                <option value="saoPaulo" >São Paulo</option>
-                <option value="saoPaulo" >Rio de Janeiro</option>
+                <select
+                name="select"
+                value={this.state.name}
+                onChange={this.handleChange}
+              >
+                <option value="Escolher" selected>Escolher</option>
+                <option value="São Paulo" >São Paulo</option>
+                <option value="Rio de Janeiro" >Rio de Janeiro</option>
                 </select> 
+            </label>
+            </div>
+
+            <div>
+            <label>
+                <input
+                type="checkbox"
+                name="validar"
+                value={this.state.name}
+                onChange={this.handleChange}
+                />
+                Gostou
             </label>
             </div>
 
